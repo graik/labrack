0f87595e544126df17baeb713767062627bcb255
Raik Gruenberg <graik@sourceforge.net>
2012-05-29 20:11:23 -0400

    complete SBOL data model requirements; work in progress

---
+++ brickit/migrations/0005_auto__add_collection.py
@@ -0,0 +1,185 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Collection'
+        db.create_table('brickit_collection', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('displayId', self.gf('django.db.models.fields.CharField')(unique=True, max_length=20)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
+            ('uri', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
+            ('shortDescription', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal('brickit', ['Collection'])
+
+        # Adding M2M table for field dnaComponents on 'Collection'
+        db.create_table('brickit_collection_dnaComponents', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('collection', models.ForeignKey(orm['brickit.collection'], null=False)),
+            ('dnacomponent', models.ForeignKey(orm['brickit.dnacomponent'], null=False))
+        ))
+        db.create_unique('brickit_collection_dnaComponents', ['collection_id', 'dnacomponent_id'])
+
+        # Adding M2M table for field proteinComponents on 'Collection'
+        db.create_table('brickit_collection_proteinComponents', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('collection', models.ForeignKey(orm['brickit.collection'], null=False)),
+            ('proteincomponent', models.ForeignKey(orm['brickit.proteincomponent'], null=False))
+        ))
+        db.create_unique('brickit_collection_proteinComponents', ['collection_id', 'proteincomponent_id'])
+
+        # Adding M2M table for field chassis on 'Collection'
+        db.create_table('brickit_collection_chassis', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('collection', models.ForeignKey(orm['brickit.collection'], null=False)),
+            ('chassis', models.ForeignKey(orm['brickit.chassis'], null=False))
+        ))
+        db.create_unique('brickit_collection_chassis', ['collection_id', 'chassis_id'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Collection'
+        db.delete_table('brickit_collection')
+
+        # Removing M2M table for field dnaComponents on 'Collection'
+        db.delete_table('brickit_collection_dnaComponents')
+
+        # Removing M2M table for field proteinComponents on 'Collection'
+        db.delete_table('brickit_collection_proteinComponents')
+
+        # Removing M2M table for field chassis on 'Collection'
+        db.delete_table('brickit_collection_chassis')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'brickit.chassis': {
+            'Meta': {'object_name': 'Chassis'},
+            'abstract': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'componentType': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.ComponentType']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'displayId': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'shortDescription': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'uri': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'db_index': 'True', 'symmetrical': 'False'}),
+            'variantOf': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.Chassis']", 'null': 'True', 'blank': 'True'})
+        },
+        'brickit.collection': {
+            'Meta': {'object_name': 'Collection'},
+            'chassis': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.Chassis']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'displayId': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'dnaComponents': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.DnaComponent']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'proteinComponents': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.ProteinComponent']", 'null': 'True', 'blank': 'True'}),
+            'shortDescription': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'uri': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'brickit.componenttype': {
+            'Meta': {'object_name': 'ComponentType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'subTypeOf': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'subTypes'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['brickit.ComponentType']"}),
+            'uri': ('django.db.models.fields.URLField', [], {'unique': 'True', 'max_length': '200'})
+        },
+        'brickit.dnacomponent': {
+            'Meta': {'object_name': 'DnaComponent'},
+            'abstract': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'componentType': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.ComponentType']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'displayId': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'optimizedFor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['brickit.Chassis']", 'null': 'True', 'blank': 'True'}),
+            'sequence': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'shortDescription': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'translatesTo': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'encodedBy'", 'null': 'True', 'to': "orm['brickit.ProteinComponent']"}),
+            'uri': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'db_index': 'True', 'symmetrical': 'False'}),
+            'variantOf': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.DnaComponent']", 'null': 'True', 'blank': 'True'})
+        },
+        'brickit.proteincomponent': {
+            'Meta': {'object_name': 'ProteinComponent'},
+            'abstract': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'componentType': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.ComponentType']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'displayId': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'sequence': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'shortDescription': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'uri': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'db_index': 'True', 'symmetrical': 'False'}),
+            'variantOf': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['brickit.ProteinComponent']", 'null': 'True', 'blank': 'True'})
+        },
+        'brickit.sample': {
+            'Meta': {'ordering': "('container', 'displayId')", 'unique_together': "(('displayId', 'container'),)", 'object_name': 'Sample'},
+            'comments': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'concentration': ('django.db.models.fields.DecimalField', [], {'default': '50', 'max_digits': '6', 'decimal_places': '2'}),
+            'concentrationUnit': ('django.db.models.fields.CharField', [], {'default': "'mg/l'", 'max_length': '10'}),
+            'container': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'samples'", 'to': "orm['brickit.SampleContainer']"}),
+            'created': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'displayId': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sampleType': ('django.db.models.fields.CharField', [], {'default': "'dna'", 'max_length': '100'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'db_index': 'True', 'symmetrical': 'False'}),
+            'wellType': ('django.db.models.fields.CharField', [], {'default': "'tube'", 'max_length': '30'})
+        },
+        'brickit.samplecontainer': {
+            'Meta': {'ordering': "('displayId',)", 'object_name': 'SampleContainer'},
+            'containerType': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'displayId': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'shortDescription': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['auth.User']", 'null': 'True', 'db_index': 'True', 'blank': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['brickit']
