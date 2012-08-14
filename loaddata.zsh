662f8094cf40ce23f049fc52bdf114a0696e5669
Raik Gruenberg <raik@iric>
2012-08-14 08:06:11 -0400

    prepare initial data snapshot with units and pre-defined parts types etc.

---
+++ loaddata.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+# load some useful initial data (units and perhaps some examples)
+# into a labrack database
+#
+# run with the shell that is configured for the django project
+# ($DJANGO_SETTINGS_MODULE and labrack in $PYTHONPATH)
+
+python manage.py loaddata initial_units.json
