e0fb70baba4cef5523d938b50a3e66f2f14ec45f
zseghiri <zseghiri@gmail.com>
2013-04-08 17:39:33 -0400

    test

--- tyers_site/settings.py
+++ tyers_site/settings.py
@@ -65,7 +65,7 @@ MEDIA_ROOT = T.approot()+  "/media"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -95,7 +95,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-#SECRET_KEY = 'hk+*n1khs%=1)p1w0r#i*yir!vx^u^rtibfb_c&bn)=5x8r19r'
+SECRET_KEY = 'hk+*n1khs%=1)p1w0r#i*yir!vx^u^rtibfb_c&bn)=5x8r19r'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
